da6d761e5b26e1cf4fbc9363c6084239783226f2
olatunjiemanuel <93155336+olatunjiemanuel@users.noreply.github.com>
2021-12-01 13:51:57 +0100

    Built UI for to do list App

--- App.js
+++ App.js
@@ -2,11 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//component imorts
+import Task from "./components/Task"
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World !</Text>
-      <StatusBar style="auto" />
+
+    <View style = {styles.taskWrapper}>
+      <Text style = {styles.sectionTitle}>Today's tasks</Text>
+
+      <View style ={styles.Items}>
+        {/*This is where the tasks will go */}
+        <Task text = "Task 1"/>
+        <Task text = "Task 2"/>
+      </View>
+
+    </View>
+
     </View>
   );
 }
@@ -14,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper:{
+    paddingTop:80,
+    paddingHorizontal:20,
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight: 'bold',
+  },
+  Items:{
+    marginTop:30,
   },
 });
